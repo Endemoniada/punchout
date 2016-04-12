f2a4fd10915dbc576bacb72ea3adbcb1cfc4deb4
Martin Jönsson <nathaniel@endemoniada.org>
2016-04-12 17:49:35 +0200

    Adds DocOpt support and version

--- punchout.py
+++ punchout.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python
 # coding=utf-8
 
+"""Punchout v0.1
+
+Usage:
+    punchout.py
+
+Options:
+    -h --help               Show this screen.
+    -v --version            Show version.
+"""
+
+from lib.docopt import docopt
+
 def main():
     print "tjo"
 
 if __name__ == '__main__':
+    arguments = docopt(__doc__, version='Punchout v0.1')
     main()
     exit()
